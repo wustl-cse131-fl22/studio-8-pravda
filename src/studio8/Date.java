eae2afacd83421c1851e40878e1a8d70edc00982
njpra <njpra@DESKTOP-ICABMGJ.wusm-wifi.wucon.wustl.edu>
2022-12-03 23:03:27 -0600

    ok

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,146 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Date {
+	private String month;
+	private int year;
+	private int day;
+	private boolean isHoliday;
+	
+	
 
 
-    public static void main(String[] args) {
+    public Date(String month, int year, int day, boolean isHoliday) {
+		super();
+		this.month = month;
+		this.year = year;
+		this.day = day;
+		this.isHoliday = isHoliday;
+	}
 
+
+
+
+	public String getMonth() {
+		return month;
+	}
+
+
+
+
+	public void setMonth(String month) {
+		this.month = month;
+	}
+
+
+
+
+	public int getYear() {
+		return year;
+	}
+
+
+
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+
+
+
+	public int getDay() {
+		return day;
+	}
+
+
+
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+
+
+
+	public boolean isHoliday() {
+		return isHoliday;
+	}
+
+
+
+
+	public void setHoliday(boolean isHoliday) {
+		this.isHoliday = isHoliday;
+	}
+
+
+
+	public String toString() {
+		if(this.isHoliday == false) {
+			return day + " " + month + " " + year + " is not a holiday";
+		}else {
+			return day + " " + month + " " + year + " is a holiday";
+		}
+		
+	}
+	
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
+
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && Objects.equals(month, other.month) && year == other.year;
+	}
+
+
+
+
+	public static void main(String[] args) {
+		Date nov27 = new Date("November", 27, 2002, true);
+		Date xmas = new Date("December", 25, 2022, true);
+		Date xmas2 = new Date("December", 25, 2022, true);
+		Date mar3 = new Date("March", 3, 1873, false);
+		Date apr19 = new Date("April", 19, 2018, false);
+		Date jul4 = new Date("July", 4, 1776, true);
+		
+		LinkedList<Date> list = new LinkedList<Date>();
+		list.add(jul4);
+		list.add(apr19);
+		list.add(nov27);
+		list.add(xmas);
+		list.add(xmas);
+		System.out.println(list);
+		
+		HashSet<Date> set = new HashSet<Date>();
+		set.add(xmas);
+		set.add(xmas2);
+		set.add(xmas);
+		System.out.println(set);
+		
+		list.add(xmas);
+		list.add(xmas2);
+		list.add(xmas);
+		System.out.println(list);
+		
+		
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,7 +1,92 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
+	private int hour;
+	private int minute;
+	private boolean is12h;
+	
+	
+
+	public Time(int hour, int minute) {
+		super();
+		this.hour = hour;
+		this.minute = minute;
+	}
+
+
+
+	public int getHour() {
+		return hour;
+	}
+
+
+
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
+
+
+	public int getMinute() {
+		return minute;
+	}
+
+
+
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
+
+
+
+	public boolean getIs12h() {
+		return is12h;
+	}
+
+
+
+	public void setHrType(boolean is12h) {
+		this.is12h = is12h;
+	}
+
+
+	public String toString() {
+		if(is12h == true) {
+			int temp = hour % 12 + 12;
+			if(hour >= 12) {
+				return temp + ":" + minute + " PM";
+			}else {
+				return temp + ":" + minute + " AM";
+			}
+		}else {
+			return hour + ":" + minute;
+		}
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, minute);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && minute == other.minute;
+	}
+
+
 
 	public static void main(String[] args) {
  
